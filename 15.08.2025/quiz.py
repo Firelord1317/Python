1abef8708241a62006068f99ee012dda723a903d
Firelord1317 <arnav.tyagi867@gmail.com>
2025-08-15 14:53:51 +0300

    Commit

---
+++ 15.08.2025/quiz.py
@@ -0,0 +1,17 @@
+answer = input("When was the first use of the word 'quiz'?")
+            
+if answer == "1781":
+  print("Correct!")
+
+else:
+ print(f"The answer is '1781', not {answer!r} ")
+
+answer = input("What is the inbuilt function that gets information from the user? ")
+
+if answer == "input":
+ print("Correct!")
+
+else:
+ print(f"The answer is 'input', not {answer!r} ")
+
+
