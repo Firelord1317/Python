1da940677791b0f5bb29c82668866de05cada5b1
Firelord1317 <arnav.tyagi867@gmail.com>
2025-09-06 20:59:01 +0300

    commit

--- 06.09.2025/assignment.py
+++ 06.09.2025/assignment.py
@@ -1,5 +1,14 @@
 import turtle
-for i in range(4):
-    turtle.forward(50)
-    turtle.right(90)
+turtle.Screen().bgcolor("Aqua")
+colors = ["red", "purple", "blue", "green", "orange", "yellow"]
+turtle.setx(0)
+turtle.sety(0)
+my_pen = turtle.Pen()
+turtle.bgcolor("black")
+
+for i in range(5):
+    my_pen.forward(200)
+    my_pen.right(90)
+    my_pen.pencolor(colors[i % 4])
+
 turtle.done()
