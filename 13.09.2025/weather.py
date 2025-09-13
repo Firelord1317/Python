3297bd7e995bf6137b0e9e0a9fa20fd4462577f1
Firelord1317 <arnav.tyagi867@gmail.com>
2025-09-13 14:38:34 +0300

    commit

---
+++ 13.09.2025/weather.py
@@ -0,0 +1,7 @@
+def weather_condition():
+    print("The weather is pleasant in", "September.", spring)
+    print('The weather is the same in autumn', autumn)
+
+spring="autumn"
+autumn="winter"
+weather_condition()
---
+++ 13.09.2025/wellwishes.py
@@ -0,0 +1,5 @@
+def well_wishes():
+    print("Hello")
+    print("Wishing you a wonderful day!")
+
+well_wishes()
