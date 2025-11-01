cd55c097dbbecd421bd40961cb0cac5484c98c1c
Firelord1317 <arnav.tyagi867@gmail.com>
2025-11-01 14:43:08 +0300

    commit

---
+++ 01.11.2025/guessthecountrycode.py
@@ -0,0 +1,86 @@
+country_code = {'India' : '0091',
+'Australia' : '0025',
+'Nepal' : '00977', 'Kenya' : '00254',
+'Canada' : '001', 'Spain' : '0034',
+'Germany' : '0049', 'Italy' : '0039', 'Iceland' : '00354', 'Japan' : '0081', 'South Korea' : '0082', 'Sweden' : '0046', 'Myanmar' : '0095', 'Mexico' : '0052', 'Brazil' : '0055', 'Argentina' : '0054', 'Colombia' : '0057', 'Peru' : '0051', 'Chile' : '0056', 'China' : '0086', 'France' : '0033', 'United Kingdom' : '0044', 'United States of America' : '001', 'Jamaica' : '001876', 'Barbados' : '001246', 'Trinidad and Tobago' : '001868'}
+
+# search dictionary for country code of India
+print("Country code for India -")
+print(country_code.get('India', 'Not Found' ))
+
+# search dictionary for country code of Angola
+print("Country code for Angola -")
+print(country_code.get('Angola', 'Not Found' ))
+
+# search dictionary for country code of Germany
+print("Country code for Germany -") 
+print(country_code.get('Germany', 'Not Found' ))
+
+# search dictionary for country code of Brazil
+print("Country code for Brazil -")  
+print(country_code.get('Brazil', 'Not Found' ))
+
+# search dictionary for country code of Japan
+print("Country code for Japan -")
+print(country_code.get('Japan', 'Not Found' ))
+
+# search dictionary for country code of Canada
+print("Country code for Canada -")
+print(country_code.get('Canada', 'Not Found' ))
+
+# search dictionary for country code of Italy
+print("Country code for Italy -")
+print(country_code.get('Italy', 'Not Found' ))
+
+# search dictionary for country code of Spain
+print("Country code for Spain -")
+print(country_code.get('Spain', 'Not Found' ))
+
+# search dictionary for country code of Australia
+print("Country code for Australia -")
+print(country_code.get('Australia', 'Not Found' ))
+
+# search dictionary for country code of United Kingdom
+print("Country code for United Kingdom -")
+print(country_code.get('United Kingdom', 'Not Found' ))
+
+# search dictionary for country code of United States of America
+print("Country code for United States of America -")
+print(country_code.get('United States of America', 'Not Found' ))
+
+# search dictionary for country code of South Korea
+print("Country code for South Korea -")
+print(country_code.get('South Korea', 'Not Found' ))
+
+# search dictionary for country code of China
+print("Country code for China -")
+print(country_code.get('China', 'Not Found' ))
+
+# search dictionary for country code of France
+print("Country code for France -")
+print(country_code.get('France', 'Not Found' ))
+
+# search dictionary for country code of Mexico
+print("Country code for Mexico -")
+print(country_code.get('Mexico', 'Not Found' ))
+
+# search dictionary for country code of Nepal
+print("Country code for Nepal -")
+print(country_code.get('Nepal', 'Not Found' ))
+
+# search dictionary for country code of Kenya
+print("Country code for Kenya -")
+print(country_code.get('Kenya', 'Not Found' ))
+
+# search dictionary for country code of Iceland
+print("Country code for Iceland -")
+print(country_code.get('Iceland', 'Not Found' ))
+
+# search dictionary for country code of Myanmar
+print("Country code for Myanmar -") 
+print(country_code.get('Myanmar', 'Not Found' ))
+
+# search dictionary for country code of Netherlands
+print("Country code for Netherlands -")
+print(country_code.get('Netherlands', 'Not Found' ))
+
