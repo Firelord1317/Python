c29b238d58ad76c5f4fbde5662068e637febfc1b
Firelord1317 <arnav.tyagi867@gmail.com>
2025-11-13 14:41:23 +0300

    commit

---
+++ 08.11.2025/assignment.py
@@ -0,0 +1,13 @@
+# Program to find the symmetric difference between two sets
+
+# Define the sets
+Set1 = {'blue', 'green'}
+Set2 = {'blue', 'yellow'}
+
+# Find symmetric difference
+sym_diff = Set1.symmetric_difference(Set2)
+
+# Display the result
+print("Set 1:", Set1)
+print("Set 2:", Set2)
+print("Symmetric Difference:", sym_diff)
